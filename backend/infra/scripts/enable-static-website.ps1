024537b7535e52d5a22bbf92e953d10768f5ce08
Aleff Morais <aleff.morais@outlook.com>
2024-01-06 18:04:23 -0300

    Change parameters to sws script

--- backend/infra/scripts/enable-static-website.ps1
+++ backend/infra/scripts/enable-static-website.ps1
@@ -1,5 +1,5 @@
-$storageAccount = Get-AzStorageAccount -ResourceGroupName
+$storageAccount = Get-AzStorageAccount -ResourceGroupName AM55-Web-Staging
 
 # Enable the static website feature on the storage account.
 $ctx = $storageAccount.Context
-Enable-AzStorageStaticWebsite -Context $ctx -IndexDocument
+Enable-AzStorageStaticWebsite -Context $ctx index.html
