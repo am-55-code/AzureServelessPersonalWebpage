6d2ee92c8fbbf168148d00e20a54e2ae04e84600
Aleff Morais <aleff.morais@outlook.com>
2024-01-10 18:11:52 -0300

    Added cdn purge script

---
+++ backend/infra/scripts/purge-cdn.sh
@@ -0,0 +1,11 @@
+# Getting the Az Storage Name
+name=$(az storage account list --resource-group AM55-Web-Prod --query "[0].name" --output tsv)
+
+# Getting the Blob Storage Container Name $web
+container=$(az storage container list --account-name $name --query "[0].name" --output tsv)
+
+# Upload Changes to Storage account
+az storage blob upload-batch --account-name $name --auth-mode key -d $container -source ./ --overwrite
+
+# Purge CDN Endpoint to replicate new changes
+ az cdn endpoint purge --content-paths  "/*" --profile-name "am55cloudsite" --name "am55cloudsite" --resource-group "AM55-Web-Staging"
