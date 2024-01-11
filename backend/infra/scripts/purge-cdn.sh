0186caa807803815d03de6971a5ebb03033e1434
Aleff Morais <aleff.morais@outlook.com>
2024-01-11 18:43:31 -0300

    fixed syntax mistake

--- backend/infra/scripts/purge-cdn.sh
+++ backend/infra/scripts/purge-cdn.sh
@@ -5,7 +5,7 @@ name=$(az storage account list --resource-group AM55-Web-Prod --query "[0].name"
 container=$(az storage container list --account-name $name --query "[0].name" --output tsv)
 
 # Upload Changes to Storage account
-az storage blob upload-batch --account-name $name --auth-mode key -d $container -source ./ --overwrite
+az storage blob upload-batch --account-name $name --auth-mode key -d $container --source ./ --overwrite
 
 # Purge CDN Endpoint to replicate new changes
  az cdn endpoint purge --content-paths  "/*" --profile-name "prdwebam55profile" --name "webprodam55" --resource-group "AM55-Web-Prod"
