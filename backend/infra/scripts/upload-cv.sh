b56dce8daad49d27e2f8192befbbc0465240077e
Aleff Morais <aleff.morais@outlook.com>
2024-01-11 17:49:26 -0300

    Small fix on cv upload script

--- backend/infra/scripts/upload-cv.sh
+++ backend/infra/scripts/upload-cv.sh
@@ -1,8 +1,11 @@
 # Getting the Az Storage Name
 name=$(az storage account list --resource-group AM55-Web-Prod --query "[0].name" --output tsv)
 
+echo $name
 # Getting the Blob Storage Container Name $cvblob
 container=$(az storage container list --account-name $name --query "[1].name" --output tsv)
+
+echo $container
 
 # Uploading CV to blob Storage
 az storage blob upload-batch --account-name $name --auth-mode key --destination $container --source ./ --overwrite
